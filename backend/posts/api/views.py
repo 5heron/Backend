4082ca1e9cc2a90aacd5a1c67dcf142da0b4635f
Sanj420 <sanjaypathiyil@gmail.com>
2024-11-05 09:26:29 +0530

    Backend added views

--- backend/posts/api/views.py
+++ backend/posts/api/views.py
@@ -8,7 +8,7 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from rest_framework import viewsets
-from .models import Theater, Show, Performance, Ticket, Customer, Booking
+from ..models import Theater, Show, Performance, Ticket, Customer, Booking
 from .serializers import TheaterSerializer, ShowSerializer, PerformanceSerializer, TicketSerializer, CustomerSerializer, BookingSerializer
 
 # Register new users
